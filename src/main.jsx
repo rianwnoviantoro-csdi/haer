4d86e1f637bece489a06413c3ab0db729ec58e62
Rian W. Noviantoro <rianwnoviantoro@gmail.com>
2024-02-28 16:40:12 +0700

    list holiday

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import 'swiper/swiper-bundle.css';
-import { register } from 'swiper/element/bundle';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PublicPage } from "./pages/index.jsx";
-
-register();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/private/holiday/index.jsx
+++ src/pages/private/holiday/index.jsx
@@ -1,12 +1,47 @@
+import { CalendarDaysIcon } from "@heroicons/react/24/outline";
+import Holidays from "date-holidays";
+import dayjs from "dayjs";
 import React from "react";
 
 function Holiday() {
+  const holidays = new Holidays("ID");
+  const currentYear = dayjs().year();
+  const holidaysArray = holidays.getHolidays(currentYear);
+
   return (
-    <div className="p-4 mb-7">
+    <div className="p-4 mb-10">
       <div className="flex justify-between place-items-center">
         <div className="">
           <h1 className="font-semibold text-lg">Holiday</h1>
         </div>
+      </div>
+      {/* Holidays */}
+      <div className="my-6">
+        {holidaysArray.map((holiday, index) => (
+          <div
+            key={index}
+            className={`bg-white rounded-lg p-3 mb-3 ${
+              dayjs(holiday.date).isAfter(dayjs())
+                ? "border-l-8 border-blue-500"
+                : "border-l-8 border-gray-300"
+            }`}
+          >
+            <div className="mb-3 flex place-items-start">
+              <div className="grow flex gap-2 place-items-center">
+                <CalendarDaysIcon className="h-4 w-4" />
+                <p className="text-sm font-semibold">
+                  {dayjs(holiday.date).format("MMMM DD, YYYY")}
+                </p>
+              </div>
+              <div className="">
+                <p className="text-xs text-[#ACAFB5]">
+                  {dayjs(holiday.date).format("dddd")}
+                </p>
+              </div>
+            </div>
+            <h1 className="text-lg font-semibold">{holiday.name}</h1>
+          </div>
+        ))}
       </div>
     </div>
   );
